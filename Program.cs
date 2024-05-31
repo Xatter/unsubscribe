716d792ee5f08d68f78b3215e40ee286a4127054
Jim Wallace <jim@extroverteddeveloper.com>
2024-05-31 07:33:14 -0400

    use file caching instead

--- Program.cs
+++ Program.cs
@@ -19,37 +19,6 @@ using System.Text.Json;
 
 namespace GmailAPIExample
 {
-    public class Header
-    {
-        public string Name { get; set; }
-        public string Value { get; set; }
-        public string? ETag { get; set; }
-    }
-
-    public class Payload
-    {
-        public string? Body { get; set; }
-        public string? Filename { get; set; }
-        public List<Header> Headers { get; set; }
-        public string MimeType { get; set; }
-        public string? PartId { get; set; }
-        public string? Parts { get; set; }
-        public string? ETag { get; set; }
-    }
-
-    public class MessageInfo
-    {
-        public int HistoryId { get; set; }
-        public string Id { get; set; }
-        public long InternalDate { get; set; }
-        public List<string> LabelIds { get; set; }
-        public Payload Payload { get; set; }
-        public string? Raw { get; set; }
-        public int SizeEstimate { get; set; }
-        public string Snippet { get; set; }
-        public string ThreadId { get; set; }
-        public string? ETag { get; set; }
-    }
     class Program
     {
         const string EMAIL_ADDRESS = "jfwallac@gmail.com";
@@ -127,10 +96,7 @@ namespace GmailAPIExample
         static IList<Message> FetchAllMessages(GmailService service, string folderId)
         {
             var result = new List<Message>();
-
-
             string? nextPageToken = String.Empty;
-
             BatchRequest batch = new BatchRequest(service);
 
             do
@@ -159,27 +125,7 @@ namespace GmailAPIExample
                             File.AppendAllLines("allMessages.json", new List<string> { JsonSerializer.Serialize(msg) });
                             result.Add(msg);
                         }
-
-
-                        // batch.Queue<Message>(getRequest, (content, error, i, httpresponse) =>
-                        // {
-                        //     if (error != null)
-                        //     {
-                        //         Console.WriteLine($"Error retrieving message {message.Id}: {error.Message}");
-                        //     }
-                        //     else
-                        //     {
-                        //         fileQueue.Enqueue(new MessageInfo{
-                        //             Id = content.Id,
-                        //             Headers = content.Payload.Headers
-                        //         });
-
-                        //         result.Add((content.Id, content.Payload.Headers));
-                        //     }
-                        // });
                     }
-
-                    // batch.ExecuteAsync().Wait();
                 }
 
                 nextPageToken = response?.NextPageToken;
@@ -226,91 +172,108 @@ namespace GmailAPIExample
                 return;
             }
 
-            var allMessages = new List<MessageInfo>();
+            IList<Message> allMessages = new List<Message>();
 
             if (File.Exists("allMessages.json"))
             {
                 foreach (var line in File.ReadAllLines("allMessages.json"))
                 {
-                    var record = JsonSerializer.Deserialize<MessageInfo>(line);
+                    var record = JsonSerializer.Deserialize<Message>(line);
                     allMessages.Add(record);
                 }
             }
-
-            // var allMessages = FetchAllMessages(service, folderId);
-            // // Save them for later, because it takes a long time to load them all
-            // var json = System.Text.Json.JsonSerializer.Serialize(allMessages);
-            // File.WriteAllText("allMessages.json", json);
+            else
+            {
+                allMessages = FetchAllMessages(service, folderId);
+            }
 
             var toUnsubscribe = allMessages.Where(msg => msg.Payload.Headers.FirstOrDefault(header => header.Name.Equals("List-Unsubscribe", StringComparison.OrdinalIgnoreCase)) != null);
             var toDelete = allMessages.Where(msg => msg.Payload.Headers.FirstOrDefault(header => header.Name.Equals("List-Unsubscribe", StringComparison.OrdinalIgnoreCase)) == null);
 
-	    var batchDeleteRequest = new BatchDeleteMessagesRequest
+            var batchDeleteRequest = new BatchDeleteMessagesRequest
             {
                 Ids = toDelete.Select(m => m.Id).ToList()
-		};
+            };
             service.Users.Messages.BatchDelete(batchDeleteRequest, "me");
 
             Console.WriteLine($"Found {allMessages.Count()} messages in folder: {folderId}");
-            Console.WriteLine($"Found {toDelete.Count()} messages that do not have List-Unsubscribe header... deleting");
+            {
+                Console.WriteLine($"Found {toDelete.Count()} messages that do not have List-Unsubscribe header... deleting");
+                var deleteRequest = new BatchDeleteMessagesRequest
+                {
+                    Ids = toDelete.Select(m => m.Id).ToList()
+                };
+                service.Users.Messages.BatchDelete(deleteRequest, "me");
+            }
 
-            var groupedByHeader = toUnsubscribe.GroupBy(msg =>{
-               var fromHeader = msg.Payload.Headers.FirstOrDefault(header => header.Name.Equals("From", StringComparison.OrdinalIgnoreCase));
-		var replyHeader = msg.Payload.Headers.FirstOrDefault(header => header.Name.Equals("Reply-To", StringComparison.OrdinalIgnoreCase));
-		
-		if (fromHeader != null) {
-		return fromHeader.Value;}
-		else {return replyHeader.Value;}
-}
-            );
+            var groupedByHeader = toUnsubscribe.GroupBy(msg =>
+            {
+                var fromHeader = msg.Payload.Headers.FirstOrDefault(header => header.Name.Equals("From", StringComparison.OrdinalIgnoreCase));
+                var replyHeader = msg.Payload.Headers.FirstOrDefault(header => header.Name.Equals("Reply-To", StringComparison.OrdinalIgnoreCase));
+
+                if (fromHeader != null) { return fromHeader.Value; }
+                else if (replyHeader != null) { return replyHeader.Value; }
+                else { return String.Empty; }
+            }).OrderByDescending(g => g.Count());
+
+            HashSet<string> seen = new HashSet<string>();
+            if (File.Exists("seen.json"))
+                seen = new HashSet<string>(File.ReadAllLines("seen.json"));
 
             foreach (var group in groupedByHeader)
             {
-		var firstMessage = group.FirstOrDefault();
-		var unsubscribeHeader = firstMessage.Payload.Headers.FirstOrDefault(header => header.Name.Equals("List-Unsubscribe", StringComparison.OrdinalIgnoreCase));
-                var values = unsubscribeHeader.Value.Split(",").Select(x => ExtractUnsubscribeUrl(x));
-                foreach (var unsubscribeUrl in values)
+                if (seen.Contains(group.Key))
                 {
-                    if (unsubscribeUrl.StartsWith("mailto"))
+                    Console.WriteLine($"Already processed {group.Key}, skipping ....... ");
+                    BatchDelete(service, group.Select(m => m.Id).ToList());
+                    continue;
+                }
+
+                var firstMessage = group.FirstOrDefault();
+                var unsubscribeHeader = firstMessage?.Payload.Headers.FirstOrDefault(header => header.Name.Equals("List-Unsubscribe", StringComparison.OrdinalIgnoreCase));
+                var values = unsubscribeHeader?.Value.Split(",").Select(x => ExtractUnsubscribeUrl(x));
+                if (values != null)
+                {
+                    foreach (var unsubscribeUrl in values)
                     {
-                        SendUnsubscribeEmail(service, unsubscribeUrl);
-                        Console.WriteLine($"Unsubscribe email sent for message with List-Unsubscribe: {group.Key}");
-                    }
-                    else
-                    {
-                        try
+                        if (!string.IsNullOrEmpty(unsubscribeUrl))
                         {
-                            HttpClient client = new HttpClient();
-                            client.GetAsync(unsubscribeUrl).Wait();
-                            Console.WriteLine($"Unsubscribe URL visited {unsubscribeUrl}");
-                        }
-                        catch (Exception e)
-                        {
-                            Console.WriteLine($"Error while visiting URL: {unsubscribeUrl}: {e}");
+
+                            if (unsubscribeUrl.StartsWith("mailto"))
+                            {
+                                SendUnsubscribeEmail(service, unsubscribeUrl);
+                                Console.WriteLine($"Unsubscribe email sent for message with List-Unsubscribe: {group.Key}");
+                            }
+                            else
+                            {
+                                try
+                                {
+                                    HttpClient client = new HttpClient();
+                                    client.GetAsync(unsubscribeUrl).Wait();
+                                    Console.WriteLine($"Unsubscribe URL visited {unsubscribeUrl}");
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine($"Error while visiting URL: {unsubscribeUrl}: {e}");
+                                }
+                            }
                         }
                     }
                 }
 
-                var deleteRequest = new BatchDeleteMessagesRequest
-                {
-                    Ids = group.Select(m => m.Id).ToList()
-                };
-                service.Users.Messages.BatchDelete(batchDeleteRequest, "me");
+                BatchDelete(service, group.Select(m => m.Id).ToList());
+                File.AppendAllLines("seen.json", new List<string>() { group.Key });
             }
+
+            File.Delete("allMessages.json");
         }
 
-        static void DeleteMail(GmailService service, Message message)
-        {
-            try
+        static void BatchDelete(GmailService service, IList<string> ids) {
+            var deleteRequest = new BatchDeleteMessagesRequest
             {
-                Console.WriteLine($"Deleting Message {message.Id}");
-                var deleteRequest = service.Users.Messages.Delete("me", message.Id);
-                Safe(deleteRequest.Execute);
-            }
-            catch (Google.GoogleApiException e)
-            {
-                Console.WriteLine($"Could not delete {message.Id}: {e}");
-            }
+                Ids = ids
+            };
+            service.Users.Messages.BatchDelete(deleteRequest, "me");
         }
 
         static string GetFolderId(GmailService service, string folderName)
